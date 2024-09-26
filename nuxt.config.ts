75f1fd896babbdae48b62df8ea01bab635115f11
linhtetuan.jvb <tuan.linh@jvb-corp.com>
2024-09-26 18:48:52 +0700

    add pinia and persist state library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,7 @@
 export default defineNuxtConfig({
   extends: ['@nuxt/ui-pro'],
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/ui',
-    '@vueuse/nuxt'
-  ],
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/ui', '@vueuse/nuxt', '@pinia/nuxt', 'pinia-plugin-persistedstate'],
 
   devServer: {
     port: 8000
